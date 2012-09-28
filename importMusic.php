a454fb9e60326b7057f161af4f910453adca658f
Baker Bokorney <bbokorney@gmail.com>
2012-09-28 00:56:29 -0400

    Fixed bugs in the import music function.

--- importMusic.php
+++ importMusic.php
@@ -5,7 +5,7 @@ $errorOut = "Errors are as follows: <br>";
 //check if the new music directory exists
 if( !is_dir("newmusic") ) {
 	//print("Error: New music directory cannot be found.<br><br>");
-	$errorOut .= "Error: New music directory cannot be found.<br>";
+	exit ("Error: New music directory cannot be found.<br>");
 }
 
 //initialize ID3 tag reader
@@ -32,26 +32,6 @@ foreach($files as $f) {
 	}
 }
 echo $errorOut;
-
-/* if(file_exists("import_error.txt")) {
-	
-}
- */
-
-/* print_r($files);
-print("<br>"); */
-
-/* $info = $getID3->analyze('newmusic/' . $files[2]);
-getid3_lib::CopyTagsToComments($info);
-echo 'file path = ' . $info['filenamepath'] . '<br>';
-echo 'artist = ' . $info['comments_html']['artist'][0] . '<br>';
-echo 'album = ' . $info['comments_html']['album'][0] . '<br>';
-echo 'title = ' . $info['comments_html']['title'][0] . '<br>';
-echo 'genre = ' . $info['comments_html']['genre'][0] . '<br>';
-echo 'length = ' . $info['playtime_string'] . '<br>';
-echo 'length_num = ' . $info['playtime_seconds'] . '<br>';
- */
-
 ?>
 <?php
 //store files into their proper location in the library
@@ -66,7 +46,6 @@ function storeFile($info) {
 	
 	//check that file has artist and album info
 	if($artist == "") {
-		echo "Artist name was empty.<br>";
 		return $filename.": Artist info could not be found.";
 	}
 	if($album == "") {
@@ -75,26 +54,34 @@ function storeFile($info) {
 	
 	$destFP = "./library/".$artist;
 	//check if directory for this artist already exists
-	if(!is_dir($destFP)) {
-		if(!mkdir($destFP, 0775)) {
-			return $filename.": Unable to make artist directory.";
-		}
+	//if not, make the directory
+	if(!is_dir($destFP) && !mkdir($destFP, 0775)) {
+		return $filename.": Unable to make artist directory.";
 	}
 	
 	$destFP .= "/".$album;
 	//check if directory for this album already exists
-	if(!is_dir($destFP)) {
-		if(!mkdir($destFP, 0775)) {
-			return $filename.": Unable to make album directory.";
-		}
+	//if not, make the directory
+	if(!is_dir($destFP) !mkdir($destFP, 0775)) {
+		return $filename.": Unable to make album directory.";
 	}
 	
+	$destFP .= "/".$filename;
 	//store file in appropriate location
-	if(copy("./newmusic/".$filename, $destFP.$filename)) {
+	if(!copy("./newmusic/".$filename, $destFP)) {
 		return $filename.": Unable to move file to library.";
 	}
 	
 	//TODO add song and info to DB
+	/* This is the format of what will be added to the DB
+	echo 'file path = ' . $info['filenamepath'] . '<br>';
+	echo 'artist = ' . $info['comments_html']['artist'][0] . '<br>';
+	echo 'album = ' . $info['comments_html']['album'][0] . '<br>';
+	echo 'title = ' . $info['comments_html']['title'][0] . '<br>';
+	echo 'genre = ' . $info['comments_html']['genre'][0] . '<br>';
+	echo 'length = ' . $info['playtime_string'] . '<br>';
+	echo 'length_num = ' . $info['playtime_seconds'] . '<br>';
+	*/
 	
 	return "";
 }
