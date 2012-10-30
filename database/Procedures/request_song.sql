58cd0e3cd8503cdd2ae9243e9a01452177bb30cc
Baker Bokorney <bbokorney@ufl.edu>
2012-10-30 16:34:09 -0400

    Completed implementation of stored proc to request a song to be put into the queue.

--- database/Procedures/request_song.sql
+++ database/Procedures/request_song.sql
@@ -1,7 +1,70 @@
 delimiter $$
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `request_song`()
-BEGIN
+CREATE DEFINER=`root`@`localhost` PROCEDURE `request_song`(
+	IN song_id int(11),
+	IN t_num int(11),
+	IN t_code int(11),
+	IN req_type tinyint(1),
+	OUT success tinyint(1),
+	OUT message nvarchar(255)
+)
+proc:BEGIN
+	DECLARE rc int;
+	SET rc = (SELECT COUNT(*) FROM payment WHERE (table_num = t_num AND id_num = t_code));
+	
+	-- check that the table and table code match
+	IF(rc != 1) THEN
+		SET success = 1;
+		-- if not, check if this table has been given a code
+		SET rc = (SELECT COUNT(*) FROM payment WHERE table_num = t_num);
+		IF(rc < 1) THEN
+			SET message = "This table does not have a code yet.";
+		ELSE
+			SET message = "Incorrect table code for this table number.";
+		END IF;		
+		LEAVE proc;
+	END IF;
 
+	-- check if paid request or vote
+	IF(req_type = 0) THEN -- if request is paid
+		-- check if song is already in queue
+		SET rc = (SELECT COUNT(*) FROM queue WHERE songID = song_id);
+		IF(rc > 0) THEN
+			-- check if this song is paid request
+			SET rc = (SELECT request_type FROM queue WHERE songID = song_id);
+			IF(rc = 0) THEN -- the song is in the queue and a paid request
+				SET success = 1;
+				SET message = "The requested song is already in the queue.";
+				LEAVE proc;	
+			ELSE -- this song an unpaid song in the queue, make it a paid request
+				UPDATE queue SET request_type = 0, priority = 0, time_requested = NOW() WHERE songID = song_id;
+			END IF;
+		ELSE -- this song is not in queue, insert song into queue
+			INSERT INTO queue VALUES (song_id, 0, 0, NOW());
+		END IF;		
+		-- increase the number of requests this table has made
+		UPDATE payment SET num_requests = num_requests+1 WHERE table_num = t_num;
+	
+	ELSE -- if requeest is a vote
+		-- check that this song is in the queue
+		SET rc = (SELECT COUNT(*) FROM queue WHERE songID = song_id);
+		IF(rc < 1) THEN
+			SET success = 1;
+			SET message = "The requested song to vote on is not in queue.";
+			LEAVE proc;
+		END IF;
+		-- check if this song is a paid request
+		SET rc = (SELECT request_type FROM queue WHERE songID = song_id);
+		IF(rc = 0) THEN
+			SET success = 1;
+			SET message = "The requested song to vote on is already a paid request.";
+			LEAVE proc;
+		END IF;
+		-- increase the priority of this song
+		UPDATE queue SET priority = priority+1 WHERE songID = song_id;
+	END IF;
+
+	SET success = 0;
+	SET message = "Success!";
 END$$
 
--- database/Procedures/test_request_song.sql
+++ database/Procedures/test_request_song.sql
@@ -1,3 +1,209 @@
 use droidbox;
 
-CALL request_song();
+DROP TABLE IF EXISTS output;
+CREATE TEMPORARY TABLE output (
+	success int,
+	message varchar(255),
+	row_count int,
+	song_id int,
+	priority int,
+	request_type int,
+	time_requested datetime,
+	table_id int,
+	num_requests int
+);
+SET @t_num = 0;
+SET @t_code = 0;
+SET @t_code0 = 0;
+SET @t_code1 = 0;
+SET @t_num_invalid = -1;
+SET @t_code_invalid = -1;
+SET @req_type = 0;
+SET @req_type_paid = 0;
+SET @req_type_unpaid = 1;
+SET @song_id = 1;
+-- create songs with ids 1 through 6
+INSERT INTO song VALUES (0, "title", "artist", "album", "rock", 0, 0, "filepath", 1);
+INSERT INTO song VALUES (0, "title", "artist", "album", "rock", 0, 0, "filepath", 1);
+INSERT INTO song VALUES (0, "title", "artist", "album", "rock", 0, 0, "filepath", 1);
+INSERT INTO song VALUES (0, "title", "artist", "album", "rock", 0, 0, "filepath", 1);
+INSERT INTO song VALUES (0, "title", "artist", "album", "rock", 0, 0, "filepath", 1);
+INSERT INTO song VALUES (0, "title", "artist", "album", "rock", 0, 0, "filepath", 1);
+
+SET @t_num = 0;
+CALL open_table(@t_num, @success, @message, @t_code0);
+-- open table for table number 1
+SET @t_num = 1;
+CALL open_table(@t_num, @success, @message, @t_code1);
+
+-- request a song not in queue (normal)
+SET @song_id = 1;
+SET @t_num = 0;
+SET @t_code = @t_code0;
+SET @req_type = @req_type_paid;
+CALL request_song(@song_id, @t_num, @t_code, @req_type, @success, @message);
+INSERT INTO output VALUES (
+	@success,
+	@message,
+	(SELECT COUNT(*) FROM queue),
+	@song_id,
+	(SELECT priority FROM queue WHERE @song_id = songID),
+	(SELECT request_type FROM queue WHERE @song_id = songID),
+	(SELECT time_requested FROM queue WHERE @song_id = songID),
+	@t_num,
+	(SELECT num_requests FROM payment WHERE @t_num = table_num)
+);
+
+-- request another song not in the queue (normal)
+SET @song_id = 2;
+CALL request_song(@song_id, @t_num, @t_code, @req_type, @success, @message);
+INSERT INTO output VALUES (
+	@success,
+	@message,
+	(SELECT COUNT(*) FROM queue),
+	@song_id,
+	(SELECT priority FROM queue WHERE @song_id = songID),
+	(SELECT request_type FROM queue WHERE @song_id = songID),
+	(SELECT time_requested FROM queue WHERE @song_id = songID),
+	@t_num,
+	(SELECT num_requests FROM payment WHERE @t_num = table_num)
+);
+
+-- vote on a song in queue (normal)
+-- insert an unpaid request into the queue to be voted on
+INSERT INTO queue VALUES (3, 0, 1, NOW());
+SET @song_id = 3;
+SET @req_type = @req_type_unpaid;
+CALL request_song(@song_id, @t_num, @t_code, @req_type, @success, @message);
+INSERT INTO output VALUES (
+	@success,
+	@message,
+	(SELECT COUNT(*) FROM queue),
+	@song_id,
+	(SELECT priority FROM queue WHERE @song_id = songID),
+	(SELECT request_type FROM queue WHERE @song_id = songID),
+	(SELECT time_requested FROM queue WHERE @song_id = songID),
+	@t_num,
+	(SELECT num_requests FROM payment WHERE @t_num = table_num)
+);
+
+-- request a song not in the queue as user 1 (normal)
+SET @song_id = 4;
+SET @t_num = 1;
+SET @t_code = @t_code1;
+SET @req_type = @req_type_paid;
+CALL request_song(@song_id, @t_num, @t_code, @req_type, @success, @message);
+INSERT INTO output VALUES (
+	@success,
+	@message,
+	(SELECT COUNT(*) FROM queue),
+	@song_id,
+	(SELECT priority FROM queue WHERE @song_id = songID),
+	(SELECT request_type FROM queue WHERE @song_id = songID),
+	(SELECT time_requested FROM queue WHERE @song_id = songID),
+	@t_num,
+	(SELECT num_requests FROM payment WHERE @t_num = table_num)
+);
+
+--  vote on an unpaid song with table 1 (normal)
+SET @song_id = 3;
+SET @req_type = @req_type_unpaid;
+CALL request_song(@song_id, @t_num, @t_code, @req_type, @success, @message);
+INSERT INTO output VALUES (
+	@success,
+	@message,
+	(SELECT COUNT(*) FROM queue),
+	@song_id,
+	(SELECT priority FROM queue WHERE @song_id = songID),
+	(SELECT request_type FROM queue WHERE @song_id = songID),
+	(SELECT time_requested FROM queue WHERE @song_id = songID),
+	@t_num,
+	(SELECT num_requests FROM payment WHERE @t_num = table_num)
+);
+
+-- request song not in queue with invalid table number and table code (error)
+SET @song_id = 5;
+SET @req_type = @req_type_paid;
+SET @t_code = @t_code_invalid;
+CALL request_song(@song_id, @t_num, @t_code, @req_type, @success, @message);
+INSERT INTO output VALUES (
+	@success,
+	@message,
+	(SELECT COUNT(*) FROM queue),
+	@song_id,
+	(SELECT priority FROM queue WHERE @song_id = songID),
+	(SELECT request_type FROM queue WHERE @song_id = songID),
+	(SELECT time_requested FROM queue WHERE @song_id = songID),
+	@t_num,
+	(SELECT num_requests FROM payment WHERE @t_num = table_num)
+);
+
+-- vote with invalid table number and table code (error)
+SET @t_num = @t_num_invalid;
+CALL request_song(@song_id, @t_num, @t_code, @req_type, @success, @message);
+INSERT INTO output VALUES (
+	@success,
+	@message,
+	(SELECT COUNT(*) FROM queue),
+	@song_id,
+	(SELECT priority FROM queue WHERE @song_id = songID),
+	(SELECT request_type FROM queue WHERE @song_id = songID),
+	(SELECT time_requested FROM queue WHERE @song_id = songID),
+	@t_num,
+	(SELECT num_requests FROM payment WHERE @t_num = table_num)
+);
+
+-- request a paid song in queue (error)
+SET @t_num = 0;
+SET @t_code = @t_code0;
+SET @song_id = 1;
+CALL request_song(@song_id, @t_num, @t_code, @req_type, @success, @message);
+INSERT INTO output VALUES (
+	@success,
+	@message,
+	(SELECT COUNT(*) FROM queue),
+	@song_id,
+	(SELECT priority FROM queue WHERE @song_id = songID),
+	(SELECT request_type FROM queue WHERE @song_id = songID),
+	(SELECT time_requested FROM queue WHERE @song_id = songID),
+	@t_num,
+	(SELECT num_requests FROM payment WHERE @t_num = table_num)
+);
+
+-- vote on a paid song in queue (error)
+SET @req_type = @req_type_unpaid;
+CALL request_song(@song_id, @t_num, @t_code, @req_type, @success, @message);
+INSERT INTO output VALUES (
+	@success,
+	@message,
+	(SELECT COUNT(*) FROM queue),
+	@song_id,
+	(SELECT priority FROM queue WHERE @song_id = songID),
+	(SELECT request_type FROM queue WHERE @song_id = songID),
+	(SELECT time_requested FROM queue WHERE @song_id = songID),
+	@t_num,
+	(SELECT num_requests FROM payment WHERE @t_num = table_num)
+);
+
+-- vote on song not in queue (error)
+SET @song_id = -1;
+CALL request_song(@song_id, @t_num, @t_code, @req_type, @success, @message);
+INSERT INTO output VALUES (
+	@success,
+	@message,
+	(SELECT COUNT(*) FROM queue),
+	@song_id,
+	(SELECT priority FROM queue WHERE @song_id = songID),
+	(SELECT request_type FROM queue WHERE @song_id = songID),
+	(SELECT time_requested FROM queue WHERE @song_id = songID),
+	@t_num,
+	(SELECT num_requests FROM payment WHERE @t_num = table_num)
+);
+
+
+SELECT * FROM output;
+DROP TABLE output;
+-- truncate payment;
+truncate song;
+truncate queue;
+truncate payment;
