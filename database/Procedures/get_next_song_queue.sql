0a16d06df928c10479e1006e28acc8297d6e72f3
Baker Bokorney <bbokorney@ufl.edu>
2012-11-02 11:07:21 -0400

    Completed implementation of get_next_song_queue stored proc.

--- database/Procedures/get_next_song_queue.sql
+++ database/Procedures/get_next_song_queue.sql
@@ -2,11 +2,30 @@ delimiter $$
 
 CREATE DEFINER=`root`@`localhost` PROCEDURE `get_next_song_queue`(
 	IN prev_song_id int(11),
-	OUT success int,
-	OUT message varchar(255),
 	OUT next_song_id int(11)
 )
 BEGIN
-
+	DECLARE cooldown int;
+	SET cooldown = -3;
+	-- increment the number of times played for previous song
+	UPDATE song SET num_played = num_played + 1 WHERE id = prev_song_id;
+	-- set the cooldown period for the previous song, change request type to cooldown
+	-- if this was a paid request
+	IF((SELECT request_type FROM queue WHERE songID = prev_song_id) = 0) THEN
+		UPDATE queue SET priority = cooldown - 1, request_type = 2 WHERE songID = prev_song_id;
+	-- if this song was unpaid
+	ELSE
+		UPDATE queue SET priority = cooldown - 1 WHERE songID = prev_song_id;
+	END IF;
+	-- increment the priorities of the songs with negative priorities
+	UPDATE queue SET priority = priority+1 WHERE priority < 0;
+	-- remove all paid songs with a 0 priority
+	DELETE FROM queue WHERE priority = 0 AND request_type = 2;
+	-- select the next song to be played
+	SET next_song_id = (SELECT songID FROM queue ORDER BY 
+						request_type, 
+						priority DESC, 
+						time_requested
+						LIMIT 1);
 END$$
 
--- database/Procedures/test_get_next_song_queue.sql
+++ database/Procedures/test_get_next_song_queue.sql
@@ -1,21 +1,12 @@
 USE droidbox;
 
-SET show_queue = 1;
-
 DROP TABLE IF EXISTS output;
 CREATE TEMPORARY TABLE output (
-	success int,
-	message varchar(255),
 	prev_song_id int,
 	next_song_id int
 );
 
 -- clear song and queue tables
-	songID int,
-	priority int,
-	request_type int,
-	time_requested datetime
-);
 TRUNCATE queue;
 TRUNCATE song;
 -- insert songs 1-6 into the library
@@ -27,23 +18,68 @@ INSERT INTO song VALUES (0, "title", "artist", "album", "rock", 0, 0, "filepath"
 INSERT INTO song VALUES (0, "title", "artist", "album", "rock", 0, 0, "filepath", 1);
 
 -- insert test songs into queue
+-- sleeps are to make sure songs have different times requested
 -- insert paid songs
-INSERT INTO queue VALUES (1, -3, 0, now());
-INSERT INTO queue VALUES (2, 0, 0, now());
-INSERT INTO queue VALUES (3, 0, 0, now());
+INSERT INTO queue VALUES (1, -3, 2, now());
+SELECT SLEEP(1);
+INSERT INTO queue VALUES (2, 1, 0, now());
+SELECT SLEEP(1);
+INSERT INTO queue VALUES (3, 1, 0, now());
+SELECT SLEEP(1);
 
 -- insert unpaid songs
 INSERT INTO queue VALUES (4, 0, 1, now());
-INSERT INTO queue VALUES (5, 1, 1, now());
-INSERT INTO queue VALUES (6, 2, 1, now());
+SELECT SLEEP(1);
+INSERT INTO queue VALUES (5, 0, 1, now());
+SELECT SLEEP(1);
+INSERT INTO queue VALUES (6, 0, 1, now());
+SELECT SLEEP(1);
 
 SET @prev_song_id = 0;
 SET @next_song_id = 0;
 
 SET @prev_song_id = 2;
-CALL get_next_song_queue(@prev_song_id, @success, @message, @next_song_id);
-INSERT INTO output VALUES (@success, @message, @next_song_id, @prev_song_id);
+CALL get_next_song_queue(@prev_song_id, @next_song_id);
+INSERT INTO output VALUES (@prev_song_id, @next_song_id);
 SET @prev_song_id = @next_song_id;
+SELECT * FROM queue;
 
-SELECT * FROM (output, queue, song);
+CALL get_next_song_queue(@prev_song_id, @next_song_id);
+INSERT INTO output VALUES (@prev_song_id, @next_song_id);
+SET @prev_song_id = @next_song_id;
+SELECT * FROM queue;
+
+CALL get_next_song_queue(@prev_song_id, @next_song_id);
+INSERT INTO output VALUES (@prev_song_id, @next_song_id);
+SET @prev_song_id = @next_song_id;
+SELECT * FROM queue;
+
+CALL get_next_song_queue(@prev_song_id, @next_song_id);
+INSERT INTO output VALUES (@prev_song_id, @next_song_id);
+SET @prev_song_id = @next_song_id;
+SELECT * FROM queue;
+
+CALL get_next_song_queue(@prev_song_id, @next_song_id);
+INSERT INTO output VALUES (@prev_song_id, @next_song_id);
+SET @prev_song_id = @next_song_id;
+SELECT * FROM queue;
+
+CALL get_next_song_queue(@prev_song_id, @next_song_id);
+INSERT INTO output VALUES (@prev_song_id, @next_song_id);
+SET @prev_song_id = @next_song_id;
+SELECT * FROM queue;
+
+CALL get_next_song_queue(@prev_song_id, @next_song_id);
+INSERT INTO output VALUES (@prev_song_id, @next_song_id);
+SET @prev_song_id = @next_song_id;
+SELECT * FROM queue;
+
+CALL get_next_song_queue(@prev_song_id, @next_song_id);
+INSERT INTO output VALUES (@prev_song_id, @next_song_id);
+SET @prev_song_id = @next_song_id;
+SELECT * FROM queue;
+
+TRUNCATE queue;
+TRUNCATE songs;
+SELECT * FROM output;
 DROP TABLE output;
