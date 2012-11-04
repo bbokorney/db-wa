648f2d25c4244d75d3c2a3d4895674142ccb9432
Chris Marchini <cmarchini@ufl.edu>
2012-11-04 16:04:46 -0500

    'Global Constants' Table.
    i.e. set a global constant to true so that the queue is cleared before
    adding more songs.

--- database/Procedures/test_play_playlist.sql
+++ database/Procedures/test_play_playlist.sql
@@ -1,26 +1,34 @@
 -- queue in the beginning
 select * from queue;
+-- Make it so that queue is erased before adding a playlist to the queue
+UPDATE constants SET constantValue := 1 WHERE constantKey = "clear_queue";
+-- view current constants
+SELECT * from constants;
 
--- delete everything in queue, then add playlist 1
-CALL play_playlist(1, 1, @success, @message);
+-- add playlist 1 
+CALL play_playlist(1, @success, @message);
 select @message;
 select @success;
 select * from queue;
 
--- delete everything in queue, then add playlist 2
-CALL play_playlist(1, 2, @success, @message);
+-- add playlist 2
+CALL play_playlist(2, @success, @message);
 select @message;
 select @success;
 select * from queue;
 
--- don't delete anything in the queue, add playlist 1
-CALL play_playlist(0, 1, @success, @message);
+-- Make it so that queue is not erased when adding a playlist to the queue
+UPDATE constants SET constantValue := 0 WHERE constantKey = "clear_queue";
+SELECT * from constants;
+
+-- add playlist 1
+CALL play_playlist(1, @success, @message);
 select @message;
 select @success;
 select * from queue;
 
 -- try to add playlist that doesn't exist
-CALL play_playlist(1, 44, @success, @message);
+CALL play_playlist(44, @success, @message);
 select @message;
 select @success;
 select * from queue;
