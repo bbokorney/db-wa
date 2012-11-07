d19b8372a5dbb0f1d5a00fa9935f6c636cbb5b9f
Chris Marchini <cmarchini@ufl.edu>
2012-11-07 16:00:47 -0500

    Update request_song and cancel_song
    You can track who requested song in queue.  These procedures now reflect
    that

--- database/Procedures/cancel_request.sql
+++ database/Procedures/cancel_request.sql
@@ -2,7 +2,7 @@ delimiter $$
 
 CREATE DEFINER=``@`` PROCEDURE `cancel_request`(
 	IN song_id int(11),
-	IN t_num int(11),
+	IN t_code int(11),
 	IN req_type tinyint(1),
 	IN user_type int(11),					-- 0 is user, 1 is waiter or admin
 	OUT success tinyint(1),
@@ -12,21 +12,18 @@ BEGIN
 	DECLARE ch int;
 
 	-- check if song is in the queue
-	SET ch = (SELECT COUNT(*) FROM queue WHERE ( song_id = songID ));
+	SET ch = (SELECT COUNT(*) FROM queue WHERE ( song_id = songID AND t_code = requested_by));
 	IF(ch < 1) THEN
 		SET success = 1;
-		SET message = "This song is no longer in the queue, or was never selected.";
+		SET message = "The song selected by this user is no longer in the queue, or was never selected.";
 	ELSE
 		-- Determine what kind of user is attempting to cancel the request
 		IF(user_type = 1) THEN -- waitress/admin trying to remove the song
-			DELETE FROM queue WHERE song_id = songID;
-			UPDATE payment SET num_requests = num_requests-1 WHERE table_num = t_num;
+			DELETE FROM queue WHERE song_id = songID AND t_code = requested_by;
+			UPDATE payment SET num_requests = num_requests-1 WHERE table_num = t_code;
 			SET success = 0;
-			SET message = "Success!";
-		ELSE	-- user trying to remove the song.  Right now there is no way for a user to remove a song 
-				-- since a song in the queue cannot identify what table number requested it.  If you want, you can have a user 
-				-- remove any song, even if he didn't request it.  However, users should not be able to remove other people's songs! 
-				-- That's why I didn't implement that in.
+			SET message = "Song removed from queue";
+		ELSE	
 			SET success = 1;
 			SET message = "Please contact a waiter/waitress in order to cancel a song request.";
 		END IF;
