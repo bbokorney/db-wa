73395691eb6bed4b5c11edf515a6392bf6f9563c
Chris Marchini <cmarchini@ufl.edu>
2012-11-07 15:23:45 -0500

    updated test cases for opening a table

--- database/Procedures/test_open_close_table.sql
+++ database/Procedures/test_open_close_table.sql
@@ -1,15 +1,16 @@
 USE droidbox;
 
+DROP TABLE IF EXISTS output;
 CREATE TEMPORARY TABLE output (
 	success int,
 	message varchar(255),
 	num int
 );
 
-SET @table_number = 10;
+SET @table_number = 12;
 
 -- open then close a table
-CALL open_table(@table_number, @success, @message, @id_num);
+CALL open_table(@table_number, "The Marchini Family", @success, @message, @id_num);
 INSERT INTO output VALUES(@success, @message, @id_num);
 CALL close_table(@table_number, @success, @message, @num_req);
 INSERT INTO output VALUES(@success, @message, @num_req);
@@ -20,12 +21,13 @@ CALL close_table(@table_number, @success, @message, @num_req);
 INSERT INTO output VALUES(@success, @message, @num_req);
 
 -- open a table that is already open
-CALL open_table(@table_number, @success, @message, @id_num);
+CALL open_table(@table_number, "Big Bowl's table", @success, @message, @id_num);
 INSERT INTO output VALUES(@success, @message, @id_num);
-CALL open_table(@table_number, @success, @message, @id_num);
+CALL open_table(@table_number, "Dwayne", @success, @message, @id_num);
 INSERT INTO output VALUES(@success, @message, @id_num);
+SET @table_number = 1;
 CALL close_table(@table_number, @success, @message, @num_req);
-INSERT INTO output VALUES(@success, @message, @num_req);
+ INSERT INTO output VALUES(@success, @message, @num_req);
 
 -- select the output
 SELECT * FROM output;
