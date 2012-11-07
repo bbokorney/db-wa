403733ee2e69b3e5fd9a695c6092d93f09a29b6f
Baker Bokorney <bbokorney@ufl.edu>
2012-11-06 19:42:50 -0500

    Removed return value from the stored procedure. Next song to be played will be selected by the web app.

--- database/Procedures/get_next_song_queue.sql
+++ database/Procedures/get_next_song_queue.sql
@@ -1,8 +1,7 @@
 delimiter $$
 
 CREATE DEFINER=`root`@`localhost` PROCEDURE `get_next_song_queue`(
-	IN prev_song_id int(11),
-	OUT next_song_id int(11)
+	IN prev_song_id int(11)
 )
 BEGIN
 	DECLARE cooldown int;
@@ -13,19 +12,12 @@ BEGIN
 	-- if this was a paid request
 	IF((SELECT request_type FROM queue WHERE songID = prev_song_id) = 0) THEN
 		UPDATE queue SET priority = cooldown - 1, request_type = 2 WHERE songID = prev_song_id;
-	-- if this song was unpaid
 	ELSE
 		UPDATE queue SET priority = cooldown - 1 WHERE songID = prev_song_id;
 	END IF;
 	-- increment the priorities of the songs with negative priorities
 	UPDATE queue SET priority = priority+1 WHERE priority < 0;
 	-- remove all paid songs with a 0 priority
-	DELETE FROM queue WHERE priority = 0 AND request_type = 2;
-	-- select the next song to be played
-	SET next_song_id = (SELECT songID FROM queue ORDER BY 
-						request_type, 
-						priority DESC, 
-						time_requested
-						LIMIT 1);
+	DELETE FROM queue WHERE priority = 0 AND request_type = 2;	
 END$$
 
