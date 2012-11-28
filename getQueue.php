4e7dbbee094f14d4d22f65a140c3094fc8786bd4
Baker Bokorney <bbokorney@ufl.edu>
2012-11-28 08:44:25 -0500

    Added request type and number of votes to info returned in getQueue.php.

--- getQueue.php
+++ getQueue.php
@@ -3,7 +3,7 @@ $database = "droidbox";
 mysql_connect("localhost");
 @mysql_select_db($database) or die( "Unable to select database");
 //$query = "SELECT id, title, artist, album FROM song,queue WHERE id = songID ORDER BY request_type,priority DESC,time_requested";
-$query = "SELECT id,title,artist,album FROM song,queue WHERE id = songID and priority >= 0 ORDER BY request_type, priority DESC, time_requested;";
+$query = "SELECT id,title,artist,album,request_type,priority FROM song,queue WHERE id = songID and priority >= 0 ORDER BY request_type, priority DESC, time_requested;";
 $result = mysql_query($query);
 $song = array();
 if(mysql_num_rows($result) > 0) {
@@ -14,6 +14,8 @@ if(mysql_num_rows($result) > 0) {
 		$song["title"] = $row["title"];
 		$song["artist"] = $row["artist"];
 		$song["album"] = $row["album"];
+		$song["req_type"] = $row["request_type"];
+		$song["num_votes"] = $row["priority"];
 		array_push($response["songs"], $song);
 	}
 	$response["success"] = 0;
